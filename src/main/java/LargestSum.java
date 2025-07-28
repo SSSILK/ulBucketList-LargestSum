2688a72cdd2a3053bf515702b62df369f5965b1e
Daijohn Bryant <ohnntsendto@gmail.com>
2025-07-28 17:19:35 +0000

    Lab --Completed

--- src/main/java/LargestSum.java
+++ src/main/java/LargestSum.java
@@ -1,5 +1,9 @@
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+
+import org.eclipse.jetty.server.RequestLog.Collection;
 
 public class LargestSum {
     /**
@@ -10,6 +14,31 @@ public class LargestSum {
      * @return the largest possible sum of separate numbers from nums.
      */
     public int bigSum(List<Integer> nums){
-        return 0;
+        
+        if ( nums == null || nums.size() < 2){
+            return 0;
+        }
+        Collections.sort(nums);
+        int count = 0;
+        int largest = nums.get(nums.size()-1);
+        for( int num: nums){
+            if (num == largest){
+                count++;
+            }
+            
+            if (count >= 2){
+                return largest * 2;
+            }
+        }
+        for ( int i = nums.size() - 2; i >=0; i--){
+            int current = nums.get(i);
+            if (current != largest){
+                return current + largest;
+            }
+        }
+        
+       
+
+        return  0;
     }
 }
